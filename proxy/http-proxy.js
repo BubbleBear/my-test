c13bc4203b60ba5a4e4f6eade48f408b60b1ce17
雪霁 <wang.haiguang@puscene.com>
2018-02-02 17:58:03 +0800

    .

---
+++ proxy/http-proxy.js
@@ -0,0 +1,26 @@
+const http = require('http');
+const url = require('url');
+
+const PROXY_PORT = 5555;
+
+function httpProxy(cReq, cRes) {
+    let options = url.parse(cReq.url);
+    options.headers = cReq.headers;
+
+    let sReq = http.request(options, (sRes) => {
+        cRes.writeHead(sRes.statusCode, sRes.headers);
+        sRes.pipe(cRes);
+    }).on('error', (e) => {
+        console.log(e);
+    });
+
+    cReq.pipe(sReq);
+}
+
+const server = http.createServer(httpProxy).listen(PROXY_PORT);
+
+server.on('error', (e) => {
+    console.dir(e);
+})
+
+module.exports = httpProxy;
