012549ae45b85716126b33d4f9193eec3ece8166
雪霁 <wang.haiguang@puscene.com>
2018-02-05 18:22:43 +0800

    .

--- test.js
+++ test.js
@@ -1,7 +1,20 @@
-function abc() {
-    console.log('abc');
-}
+const fs = require('fs');
+const net = require('net');
 
-let x = 'abc';
+const TEST_DIR = '/Users/vatel/Downloads';
 
-[x]()
+// const fd = fs.openSync('/Users/vatel/Downloads/a.jpg', 'r');
+// const buffer = 
+// const rs = fs.readSync(fd, )
+
+const file = fs.readFileSync(`${TEST_DIR}/a.jpg`);
+
+const server = net.createServer((socket) => {
+    socket.on('data', chunk => {
+        fs.writeFileSync(`${TEST_DIR}/b.jpg`, chunk, {
+            flag: 'a+'
+        })
+    })
+}).listen('3333');
+
+const client = net.connect(3333).write(file);
