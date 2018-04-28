e4911d238b07270b881f7083eaf8bb3298d60845
雪霁 <wang.haiguang@puscene.com>
2018-04-28 16:01:30 +0800

    .

--- test.js
+++ test.js
@@ -7,11 +7,13 @@ function format(data = []) {
     data.map(item => {
         const wrappedItem = Object.assign({}, item, {_parent: null, _children: []});
         isRoot.call(wrappedItem) && roots.push(wrappedItem);
-
         categoryMap[wrappedItem.category_id] = wrappedItem;
-        wrappedItem.parent_id &&
-            categoryMap[wrappedItem.parent_id]._children.push(wrappedItem) &&
-            (wrappedItem._parent = categoryMap[wrappedItem.parent_id]);
+    })
+
+    data.map(item => {
+        item.parent_id &&
+            categoryMap[item.parent_id]._children.push(item) &&
+            (item._parent = categoryMap[item.parent_id]);
     });
 
     return {categoryMap, roots};
